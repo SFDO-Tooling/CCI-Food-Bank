a953ab0d498b3e7a093cde209c040cbeb97b3fa1
prescod <paul@prescod.net>
2022-09-28 01:44:40 +0000

    huihuihiu

---
+++ datasets/hhhh/hhhh.dataset.sql
@@ -0,0 +1,17 @@
+BEGIN TRANSACTION;
+CREATE TABLE "Account" (
+	id INTEGER NOT NULL, 
+	"Name" VARCHAR(255), 
+	PRIMARY KEY (id)
+);
+CREATE TABLE "Contact" (
+	id INTEGER NOT NULL, 
+	"DoNotCall" VARCHAR(255), 
+	"HasOptedOutOfEmail" VARCHAR(255), 
+	"HasOptedOutOfFax" VARCHAR(255), 
+	"LastName" VARCHAR(255), 
+	"AccountId" VARCHAR(255), 
+	PRIMARY KEY (id)
+);
+INSERT INTO "Contact" VALUES(1,'False','False','False','bbb','');
+COMMIT;
