c7d8abda0194442a85a20d038e70ac23ccb34706
jofsky <jkofsky@salesforce.com>
2022-10-13 20:55:13 +0000

    asdf

--- datasets/test/test.dataset.sql
+++ datasets/test/test.dataset.sql
@@ -2,7 +2,7 @@ BEGIN TRANSACTION;
 CREATE TABLE "Account" (
 	id INTEGER NOT NULL, 
 	"Name" VARCHAR(255), 
-	"Description" VARCHAR(255), 
+	"Fax" VARCHAR(255), 
 	PRIMARY KEY (id)
 );
 INSERT INTO "Account" VALUES(1,'josh kofsky','');
